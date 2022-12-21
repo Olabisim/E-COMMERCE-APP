001a31bb3e360cb1869da0b856beb4270b9540fe
holabisii <ajoseholabisi@gmail.com>
2022-12-21 10:46:25 -0500

    env variables

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
 const express = require('express'),
         app = express();
 
+require('dotenv/config')
 
-app.get('/', (req, res) => {
+const api = process.env.API_URL;
+
+
+app.get( api + '/products', (req, res) => {
         res.send('hello API !')
 })
 
 
 app.listen(3000, () => {
+        console.log(api)
         console.log('server is running on http://localhost:3000');
 })
