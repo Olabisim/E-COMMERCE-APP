870ed933aeea2d9400341c30664407ccfe3e8b8e
holabisii <ajoseholabisi@gmail.com>
2022-12-21 14:34:05 -0500

    logging api requests

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express'),
-        app = express();
+        app = express(),
+        morgan = require('morgan');
 
 require('dotenv/config')
 
@@ -7,7 +8,7 @@ const api = process.env.API_URL;
 
 // middleware 
 app.use(express.json())
-
+app.use(morgan('tiny'))
 
 app.get( api + '/products', (req, res) => {
         const product = {
