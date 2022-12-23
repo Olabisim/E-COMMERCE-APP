55fc03c5fe58f2f69ea76ed3c618295f392b3928
holabisii <ajoseholabisi@gmail.com>
2022-12-23 06:34:09 -0500

    reading and writing to database

--- backend/app.js
+++ backend/app.js
@@ -11,17 +11,37 @@ const api = process.env.API_URL;
 app.use(express.json())
 app.use(morgan('tiny'))
 
-app.get( api + '/products', (req, res) => {
-        const product = {
-                id: 1,
-                name: "hair dresser",
-                image: 'some_url'
+const productSchema = mongoose.Schema({
+        name: String,
+        image: String,
+        countInStock: {type: String, required: true}
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+app.get( api + '/products', async (req, res) => {
+        const product =  await Product.find()
+
+        if(!product) {
+                res.status(500).json({
+                        error: "error occured"
+                })
         }
-        res.send(product)
+
+        res.status(200).json({
+                success: true,
+                data: product
+        })
+
 })
 app.post( api + '/products', (req, res) => {
-        const product = req.body;
-        res.send(product)
+        const {name, image, countInStock} = req.body;
+        const product = new Product({
+                name, image, countInStock
+        })
+        product.save()
+                .then(createedProduct => res.status(201).json({createedProduct}))
+                .catch(err => res.status(500).json({err, success: false}))
 })
 
 mongoose.set('strictQuery', true);
