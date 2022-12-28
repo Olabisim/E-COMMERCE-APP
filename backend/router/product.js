f2f2c6fa8cee2184b4201377ca729ed0485a45e3
holabisii <ajoseholabisi@gmail.com>
2022-12-28 17:51:27 -0500

    get request of product refactored

--- backend/router/product.js
+++ backend/router/product.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {Product} = require('../model/Product')
+const {Category} = require('../model/category')
 
 
 router.get('/', async (_, res) => {
@@ -18,14 +19,24 @@ router.get('/', async (_, res) => {
         })
 
 })
-router.post('/', (req, res) => {
-        const {name, image, countInStock} = req.body;
+
+
+router.post('/', async (req, res) => {
+
+        const {name,description, richDescription, image, images, price, category, countInStock, rating, numReviews, isFeatured} = req.body;
+
+        const categoryCollected = await Category.findById(category)
+
+        if(!categoryCollected) res.status(400).json({success: false, message: "category cannot be  found"})
+
         const product = new Product({
-                name, image, countInStock
+                name,description, richDescription, image, images, price, category: categoryCollected, countInStock, rating, numReviews, isFeatured
         })
+
         product.save()
                 .then(createedProduct => res.status(201).json({createedProduct}))
                 .catch(err => res.status(500).json({err, success: false}))
+                
 })
 
 
