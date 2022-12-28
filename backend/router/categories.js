377d29f1830a8b6c943453c4a259b3a13cdb5aad
holabisii <ajoseholabisi@gmail.com>
2022-12-28 10:56:46 -0500

    add and delete category

--- backend/router/categories.js
+++ backend/router/categories.js
@@ -1,14 +1,36 @@
 
 const express = require('express');
 const router = express.Router();
-const Category = require('../model/category')
+const {Category} = require('../model/category')
 
 router.get('/', async (_, res) => {
         const category = await Category.find()
 
         if(!category) res.status(500).json({success: false})
 
-        res.status(200).json({success: false, data: category})
+        res.status(200).json({success: true, data: category})
+})
+
+router.post('/', async (req, res) => {
+        const {name, color, icon} = req.body;
+        
+        const category = new Category({
+                name, color, icon
+        })
+
+        await category.save()
+        if(!category) res.status(404).json({success: false, message: "category cannot be created! "})
+        
+        res.status(200).json({success: true, message: "category saved successfully to the database"})
+})
+
+router.delete('/:id', (req, res) => {
+        Category.findByIdAndRemove(req.params.id)
+                .then (category => {
+                        if(!category) res.status(404).json({success: false, message: "Category not found"})
+                        res.status(200).json({success: true, message: "category successfully removed from the database"})
+                })
+                .catch(err => res.status(400).json({success: false, error: err}))
 })
 
 
