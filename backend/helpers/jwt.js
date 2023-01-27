d54df1a846480b6c5a562aa220acf173d2d78c0d
holabisii <ajoseholabisi@gmail.com>
2023-01-27 11:34:00 -0500

    excluding api routes from authentication

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -2,10 +2,22 @@
 const {expressjwt} = require('express-jwt')
 
 function authJwt() {
+
+        const api = process.env.api
+
         return expressjwt({
                 secret: process.env.secret,
                 algorithms: ['HS256']
-        })
+        }).unless({
+                path: [
+                    {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
+                    {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
+                    {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+                    {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
+                    `${api}/users/login`,
+                    `${api}/users/register`,
+                ]
+            })
 }
 
 module.exports = authJwt
