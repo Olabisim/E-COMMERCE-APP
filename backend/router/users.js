61373e3bc9106f437467ad153b3d03244edc4653
holabisii <ajoseholabisi@gmail.com>
2023-01-27 09:08:26 -0500

    register a new user

--- backend/router/users.js
+++ backend/router/users.js
@@ -12,5 +12,27 @@ router.get('/', async (_, res) => {
         res.status(200).json({success: true, data: user})
 })
 
+router.post('/', async (req,res)=>{
+        let user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: req.body.passwordHash,
+            phone: req.body.phone,
+            isAdmin: req.body.isAdmin,
+            street: req.body.street,
+            apartment: req.body.apartment,
+            zip: req.body.zip,
+            city: req.body.city,
+            country: req.body.country,
+        })
+        
+
+        user = await user.save();
+    
+        if(!user) return res.status(400).send('the user cannot be created!')
+    
+        res.send(user);
+})
+    
 
 module.exports =  router;
