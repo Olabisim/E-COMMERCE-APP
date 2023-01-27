1516f01624ff3c4bd320318a6b3c7614d40c5eb2
holabisii <ajoseholabisi@gmail.com>
2023-01-27 09:45:48 -0500

    remove password api and getuserbyId

--- backend/router/users.js
+++ backend/router/users.js
@@ -2,10 +2,19 @@
 const express = require('express')
 const router = express.Router();
 const {User} = require('../model/user')
+const bcrypt = require('bcryptjs')
 
 
 router.get('/', async (_, res) => {
-        const user = await User.find();
+        const user = await User.find().select("-passwordHash");
+
+        if(!user) res.status(500).json({success: false})
+
+        res.status(200).json({success: true, data: user})
+})
+
+router.get('/:id', async (req, res) => {
+        const user = await User.findById(req.params.id).select("-passwordHash");
 
         if(!user) res.status(500).json({success: false})
 
@@ -16,7 +25,7 @@ router.post('/', async (req,res)=>{
         let user = new User({
             name: req.body.name,
             email: req.body.email,
-            passwordHash: req.body.passwordHash,
+            passwordHash: bcrypt.hashSync(req.body.password, 10),
             phone: req.body.phone,
             isAdmin: req.body.isAdmin,
             street: req.body.street,
