ca551f359c815a401c07a479509cef1b9bfa46fb
olabisim <ajoseholabisi@gmail.com>
2024-04-29 02:01:29 -0700

    integrated flatlist with products

--- easy-shop/Screen/Products/ProductContainer.js
+++ easy-shop/Screen/Products/ProductContainer.js
@@ -1,11 +1,34 @@
-import React, {useState} from 'react'
-import {View as V, StyleSheet, ActivityIndicator} from 'react-native'
+import React, {useEffect, useState} from 'react'
+import {View as V, StyleSheet, Text as T, ActivityIndicator, FlatList as FL } from 'react-native'
+
+const data = require('../../assets/data/products.json')
 
 const ProductContainer = () => {
+
+    const [ products, setProducts] = useState([]);
+
+    useEffect(() => {
+        setProducts(data)
+
+        return () => {
+            setProducts([])
+        }
+    }, [])
+
+
+
     return (
-        <View> 
-            <Text>Product Container</Text>
-        </View>
+        <V> 
+            <T>Product Container</T>
+            <V style={{marginTop: 100}}>
+                <FL 
+                    horizontal
+                    data={products}
+                    renderItem={({item}) => <T>{item.brand}</T>}
+                    keyExtractor={item => item.name}
+                />
+            </V>
+        </V>
     )
 }
 
