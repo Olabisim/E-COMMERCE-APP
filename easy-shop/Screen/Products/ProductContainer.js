ef4590abbe335add4d1c721a7a7358ca03a94c94
olabisim <ajoseholabisi@gmail.com>
2024-05-07 01:10:35 -0700

    setting categories

--- easy-shop/Screen/Products/ProductContainer.js
+++ easy-shop/Screen/Products/ProductContainer.js
@@ -10,22 +10,35 @@ import Banner from '../../Shared/Banner';
 var {width}  = D.get("window")
 
 const data = require('../../assets/data/products.json')
+const categories = require('../../assets/data/categories.json')
 
 const ProductContainer = () => {
 
     const [ products, setProducts] = useState([]);
     const [ productsFiltered, setProductsFiltered] = useState([]);
     const [ focus, setFocus] = useState();
+    const [ categories, setCategories] = useState([]);
+    const [ active, setActive] = useState();
+    const [ initialState, setinitialState] = useState([]);
+
 
     useEffect(() => {
         setProducts(data);
         setProductsFiltered(data);
+        setFocus(false);
+        setCategories(categories)
+        setActive(-1)
+        setinitialState(data)
 
         return () => {
             setProducts([])
             setProductsFiltered([])
             setFocus()
+            setCategories([])
+            setActive()
+            setinitialState([])
         }
+
     }, [])
 
 
