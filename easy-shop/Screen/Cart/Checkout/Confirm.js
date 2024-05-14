fba9a4910cc0f77f2b965d79bf3f1bf38ce6f037
olabisim <ajoseholabisi@gmail.com>
2024-05-14 10:16:05 -0700

    confirm page

--- easy-shop/Screen/Cart/Checkout/Confirm.js
+++ easy-shop/Screen/Cart/Checkout/Confirm.js
@@ -1,14 +1,99 @@
 import React, {useState, useEffect} from 'react'
 import { StyleSheet as SS, Image as I, SafeAreaView as SAV, View as V, Dimensions as D, Text as T, ScrollView as SV, Button as B } from 'react-native';
-// import {Left, Right, Container, H1}  from 'native-base'
+import { Text, Left, Right, ListItem, Thumbnail, Body } from "native-base";
 
+var {height, width} = D.get('window')
 
-const Confirm = () => {
+const Confirm = (props) => {
+
+    const finalOrder = props.route.params;
+
     return (
-        <V>
-            <T>Confirm Screen</T>
-        </V>
+        <SV contentContainerStyle={styles.container}>
+            <V style={styles.titleContainer}>
+                <Text style={{ fontSize: 20, fontWeight: "bold" }}>Confirm Order</Text>
+
+                {
+                props.route.params 
+                && 
+                (
+                    <V style={{ borderWidth: 1, borderColor: "orange" }}>
+                        <Text style={styles.title}>Shipping to:</Text>
+                        <V style={{ padding: 8 }}>
+                            <Text>Address: {finalOrder.order.order.shippingAddress1}</Text>
+                            <Text>Address2: {finalOrder.order.order.shippingAddress2}</Text>
+                            <Text>City: {finalOrder.order.order.city}</Text>
+                            <Text>Zip Code: {finalOrder.order.order.zip}</Text>
+                            <Text>Country: {finalOrder.order.order.country}</Text>
+                        </V>
+                        <Text style={styles.title}>Items:</Text>
+                        {/* CHANGE THIS */}
+                        {/* {productUpdate && (
+                        <>
+                            {productUpdate.map((x) => {
+                            return (
+                                <ListItem style={styles.listItem} key={x.name} avatar>
+                                    <Left>
+                                        <Thumbnail source={{ uri: x.image }} />
+                                    </Left>
+                                    <Body style={styles.body}>
+                                        <Left>
+                                        <Text>{x.name}</Text>
+                                        </Left>
+                                        <Right>
+                                        <Text>$ {x.price}</Text>
+                                        </Right>
+                                    </Body>
+                                </ListItem>
+                            );
+                            })}
+                        </>
+                        )} */}
+                    </V>
+                )   
+                }
+
+
+                <V style={{ alignItems: "center", margin: 20 }}>
+                    {/* <B title={"Place order"} onPress={confirmOrder} /> */}
+                    <B title={"Place order"} />
+                </V>
+            </V>
+        </SV>
     )
 }
+
+
+const styles = SS.create({
+    container: {
+      height: height,
+      padding: 8,
+      alignContent: "center",
+      backgroundColor: "white",
+    },
+    titleContainer: {
+      justifyContent: "center",
+      alignItems: "center",
+      margin: 8,
+    },
+    title: {
+      alignSelf: "center",
+      margin: 8,
+      fontSize: 16,
+      fontWeight: "bold",
+    },
+    listItem: {
+      alignItems: "center",
+      backgroundColor: "white",
+      justifyContent: "center",
+      width: width / 1.2,
+    },
+    body: {
+      margin: 10,
+      alignItems: "center",
+      flexDirection: "row",
+    },
+});
+
 
 export default Confirm;
