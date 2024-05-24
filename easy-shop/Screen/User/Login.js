4731054b1b2591f4c4bf87793f0b1a15c1a2ec34
olabisim <ajoseholabisi@gmail.com>
2024-05-24 02:52:34 -0700

    seterror empty on success

--- easy-shop/Screen/User/Login.js
+++ easy-shop/Screen/User/Login.js
@@ -20,6 +20,7 @@ const Login = (props) => {
             setError("Please fill in your credentials")
         }
         else {
+            setError("")
             console.log("success")
         }
     }
