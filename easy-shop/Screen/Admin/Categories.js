de94158711a57b964ae0b46bb1381747713dbb54
olabisim <ajoseholabisi@gmail.com>
2024-05-31 07:21:32 -0700

    category item component

--- easy-shop/Screen/Admin/Categories.js
+++ easy-shop/Screen/Admin/Categories.js
@@ -10,8 +10,24 @@ import {
 import EasyButton from "../../Shared/StyledComponents/EasyButton"
 import baseURL from "../../assets/common/baseUrl";
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 var { width } = Dimensions.get("window")
+
+
+const Item = (props) => {
+    return (
+        <View style={styles.item}>
+            <Text>{props.item.name}</Text>
+            <EasyButton
+                danger
+                medium
+            >
+                <Text style={{ color: "white", fontWeight: "bold"}}>Delete</Text>
+            </EasyButton>
+        </View>
+    )
+}
 
 const Categories = (props) => {
     
@@ -19,13 +35,31 @@ const Categories = (props) => {
     const [categoryName, setCategoryName] = useState();
     const [token, setToken] = useState();
 
+    useEffect(() => {
+        AsyncStorage.getItem("jwt")
+            .then((res) => {
+                setToken(res);
+            })
+            .catch((error) => console.log(error));
+
+        axios
+            .get(`${baseURL}category`)
+            .then((res) => setCategories(res.data.data))
+            .catch((error) => alert("Error to load categories"))
+
+        return () => {
+            setCategories();
+            setToken();
+        }
+    }, [])
+
     return (
         <View style={{ position: "relative", height: "100%"}}>
             <View style={{ marginBottom: 60 }}>
                 <FlatList 
                     data={categories}
                     renderItem={({ item, index }) => (
-                        <Text>{item}</Text>
+                        <Item item={item} index={index} />
                     )}
                     keyExtractor={(item) => item.id}
                 />
@@ -72,6 +106,23 @@ const styles = StyleSheet.create({
         height: 40,
         borderColor: "gray",
         borderWidth: 1
+    },
+    item: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1,
+        elevation: 1,
+        padding: 5,
+        margin: 5,
+        backgroundColor: "white",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        borderRadius: 5
     }
 })
 
