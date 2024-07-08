b7fcdcb24d12214c46d317bbad704d99e2e5ab78
Olabisim <olabisiajoseh@gmail.com>
2024-07-08 10:59:46 +0100

    orders api adjusted

--- easy-shop/Screen/Admin/Order.js
+++ easy-shop/Screen/Admin/Order.js
@@ -5,11 +5,25 @@ import baseURL from "../../assets/common/baseUrl"
 import { useFocusEffect } from "@react-navigation/native"
 import OrderCard from "../../Shared/OrderCard"
 
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
+
 const Orders = (props) => {
     
     const [orderList, setOrderList] = useState();
     
-
     useFocusEffect(
         useCallback(
             () => {
@@ -34,13 +48,16 @@ const Orders = (props) => {
 
     return (
         <View>
+            <Text>This is some data</Text>
             <FlatList 
-                data={orderList}
+                data={orderList?.data}
                 renderItem={({ item }) => (
+                    
                     <OrderCard navigation={props.navigation} {...item} editMode={true}/>
                 )}
                 keyExtractor={(item) => item.id}
             />
+            <Text>This is some data</Text>
         </View>
     )
 }
